acab6ffbc2152d6e76637cf2d4e3034f506684cf
Patrick Fisher <patrick@pwfisher.com>
2013-03-16 21:22:05 +0000

    initial commit - static server with 301 map and comments

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ node-playground
 ===============
 
 Gettting started with node.
+
+A simple web server based on node-static. with extra comments to help the novice node programmer.
+
+See http://pwfisher.com/2013/node-static-forever.html for a quick environment setup.
---
+++ front_controller.js
@@ -0,0 +1,68 @@
+
+var
+    /**
+     * Node built-in HTTP server module.
+     */
+    http = require('http'),
+
+    /**
+     * node-static: a simple, rfc 2616 compliant file streaming module for node.
+     * @see https://github.com/cloudhead/node-static
+     */
+    static = require('node-static'),
+    staticServer = new(static.Server)('../../public_html'),
+
+    /**
+     * HTTP 301 redirect map.
+     * No support for query param variations or other 3XX response codes.
+     */
+    redirectMap = {
+        '/nucleus/index.php?itemid=45': '/2009/commandline-parseargs-php-cli-options-parser.html',
+    },
+
+    /**
+     * Request listener - execution begins here. Master controller logic.
+     *
+     * @param {http.IncomingMessage} request
+     * @param {http.ServerResponse} response
+     */
+    requestListener = function(request, response)
+    {
+        request.addListener('end', function ()
+        {
+            if (redirectMap[request.url])
+            {
+                // Redirect
+                //
+                response.writeHead(301, { 'Location': redirectMap[request.url] });
+                console.log('> Redirect: ' + request.url + ' - ' + redirectMap[request.url]);
+                response.end();
+            }
+            else
+            {
+                // Static File
+                //
+                staticServer.serve(request, response, function (err, res)
+                {
+                    if (err)
+                    {
+                        console.error('> Error serving ' + request.url + ' - ' + err.message);
+                        response.writeHead(err.status, err.headers);
+                        response.end();
+                    }
+                    else // success
+                    {
+                        console.log('> ' + request.url + ' - ' + res.message);
+                    }
+                });
+            }
+        });
+    },
+
+    /**
+     * @var {http.Server}
+     */
+    httpServer = http.createServer(requestListener);
+
+
+httpServer.listen(8081);
